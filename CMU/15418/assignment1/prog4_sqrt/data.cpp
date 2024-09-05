b92b725d5cc2e38618f4995212e00aa50ee068e1
Abhiraj <abhiraj.official15@gmail.com>
2024-09-05 18:05:08 +0530

    prog4

--- CMU/15418/assignment1/prog4_sqrt/data.cpp
+++ CMU/15418/assignment1/prog4_sqrt/data.cpp
@@ -14,16 +14,22 @@ void initGood(float *values, int N) {
     for (int i=0; i<N; i++)
     {
         // Todo: Choose values
-        values[i] = 1.0f;
+        //for the value 3, we have highest Arithmetic intensitiy, 
+        //since the number of iterations to converge is max
+        values[i] = 3.0f - 0.00001f;
     }
 }
 
 // Generate data that gives low relative speedup
 void initBad(float *values, int N) {
+    // We want Arithmetic Intensity to be least, so least iterations with 1. Increases bandwith latency
+    // Along with increasing the number of iteration, now we introduce a 3 in the vector length so that the SIMD exec is bottled.
+    int vector_len = 8;
     for (int i=0; i<N; i++)
     {
         // Todo: Choose values
-        values[i] = 1.0f;
+        values[i] = 1.0f+0.00001f;
     }
+    for(int i=0; i<N; i+=vector_len) values[i] = 3.0f - 0.00001f;
 }
 
